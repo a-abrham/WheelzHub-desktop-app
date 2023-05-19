008e11e40c4a69c4b03850065637bcc66daf2e3e
abrshkorra <abrshkorra@gmail.com>
2023-05-19 15:35:40 +0300

    controller for adding the car to the database

---
+++ src/controllers/CarFormController.java
@@ -0,0 +1,81 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import DBconnection.DBhandler;
+
+public class CarFormController {
+    @FXML
+    private TextField makeField;
+    @FXML
+    private TextField modelField;
+    @FXML
+    private TextField yearField;
+    @FXML
+    private TextField colorField;
+    @FXML
+    private TextField mileageField;
+    @FXML
+    private TextField priceField;
+
+    private File selectedImage;
+    private Connection connection;
+    private DBhandler handler;
+    private PreparedStatement pst;
+
+    @FXML
+    private void selectImage() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Image");
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Image Files", "*.jpg", "*.png"));
+        selectedImage = fileChooser.showOpenDialog(null);
+    }
+
+    @FXML
+    private void submitForm() {
+        handler = new DBhandler(); // Initialize the DBhandler object
+
+        String make = makeField.getText();
+        String model = modelField.getText();
+        int year = Integer.parseInt(yearField.getText());
+        String color = colorField.getText();
+        int mileage = Integer.parseInt(mileageField.getText());
+        double price = Double.parseDouble(priceField.getText());
+
+        // Code to insert the car details and image into the database
+        try {
+            connection = handler.getConnection();
+            String insert = "INSERT INTO cars (make, model, year, color, mileage, price, image1) VALUES (?,?,?,?,?,?,?)";
+            pst = connection.prepareStatement(insert);
+            pst.setString(1, make);
+            pst.setString(2, model);
+            pst.setInt(3, year);
+            pst.setString(4, color);
+            pst.setInt(5, mileage);
+            pst.setDouble(6, price);
+            FileInputStream imageStream = new FileInputStream(selectedImage);
+            pst.setBinaryStream(7, imageStream);
+            pst.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Use the selectedImage file for further processing
+
+        // Reset the form fields after submission (optional)
+        makeField.clear();
+        modelField.clear();
+        yearField.clear();
+        colorField.clear();
+        mileageField.clear();
+        priceField.clear();
+    }
+}
