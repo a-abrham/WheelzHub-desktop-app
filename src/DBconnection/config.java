bc22b216ea3c14f3d0d79849201b6cfed8f2dbef
abrshkorra <abrshkorra@gmail.com>
2023-05-19 15:42:06 +0300

    req fields

---
+++ src/DBconnection/config.java
@@ -0,0 +1,9 @@
+package DBconnection;
+
+public class config {
+    protected static String dbhost = "localhost";
+    protected static String dbport = "3306";
+    protected static String duser = "root";
+    protected static String dpassword = "";
+    protected static String dbname = "crsusers";
+}
