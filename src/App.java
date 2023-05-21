1fd229861f1985e9ef3b4894a0dbbf6572fbf5c7
abrshkorra <abrshkorra@gmail.com>
2023-05-21 23:30:15 +0300

    added app icon

--- src/App.java
+++ src/App.java
@@ -4,11 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
    @Override
    public void start(Stage primaryStage) throws IOException {
+      Image icon = new Image(getClass().getResourceAsStream("img/car.png"));
+      primaryStage.getIcons().add(icon);
       Parent root = FXMLLoader.load(getClass().getResource("/FXML/Login.fxml"));
       Scene scene = new Scene(root, 600, 400);
       primaryStage.setScene(scene);
